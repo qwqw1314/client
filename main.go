1180b337e7a8c5fdc8a03a1e0f8313f65c746a86
qwqw1314 <qwqw1314@gmail.com>
2022-12-02 17:43:57 +0900

    v0.0.2

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	var ip string
+	file, err := os.Open("/etc/hostname")
+	if err != nil {
+		panic(err)
+	}
+	hostname := bufio.NewScanner(file)
+	hostname.Scan()
+	subtext := hostname.Text()
+	file.Close()
+
+	file, err = os.Open("/etc/hosts")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.Contains(text, subtext) {
+			ip = strings.Split(text, " ")[0]
+		}
+	}
+
+	file.Close()
+
 	for {
-		reqBody := bytes.NewBufferString("Post plain text")
-		resp, err := http.Post("loadbalancer.default.svc", "text/plain", reqBody)
+		reqBody := bytes.NewBufferString(ip)
+		resp, err := http.Post("http://loadbalancer.default.svc", "text/plain", reqBody)
 		if err != nil {
 			fmt.Println(err)
 		}
